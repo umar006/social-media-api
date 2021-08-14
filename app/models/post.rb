72e8433e0a7c7cd2ad0c2d9fc270b63ce61e5527
umar006 <umaralfaruq597@gmail.com>
2021-08-14 14:30:24 +0700

    create save method
    this method will save post to database
    
    Signed-off-by: umar006 <umaralfaruq597@gmail.com>

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,12 +1,28 @@
 require './config/env/development'
 
 class Post
-  attr_accessor :id, :post, :attach, :user_id
+  attr_accessor :id, :post, :attach, :user
 
   def initialize(id, post, attach, user)
     @id = id
     @post = post
     @attach = attach
     @user = user
+  end
+
+  def save
+    return false unless valid?
+
+    client = create_db_client
+
+    client.query("insert into posts (post, attachment, user_id) values ('#@post', '#@attach', '#{@user.id}')")
+
+    true
+  end
+
+  def valid?
+    return false if @post.nil? || @user.nil?
+
+    true
   end
 end
