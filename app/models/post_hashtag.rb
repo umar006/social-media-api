b1891ce04fc4d369d2cf2e63e71dd3e02dbe3a54
umar006 <umaralfaruq597@gmail.com>
2021-08-15 15:47:55 +0700

    add method save and last_insert_id
    save method used to insert post_id and hashtag_id to database and last_insert_id method used to get last id from certain table
    
    Signed-off-by: umar006 <umaralfaruq597@gmail.com>

--- app/models/post_hashtag.rb
+++ app/models/post_hashtag.rb
@@ -1,4 +1,19 @@
 require './config/env/development'
 
 class PostHashtag
+  def save
+    client = create_db_client
+    
+    post_id = last_insert_id('posts')
+    hashtag_id = last_insert_id('hashtags')
+
+    client.query("insert into post_hashtag values ('#{post_id}', '#{hashtag_id}')")
+  end
+
+  def last_insert_id(table_name)
+    client = create_db_client
+
+    id = client.query("select id from #{table_name} order by id desc").each.first['id']
+    id
+  end
 end
