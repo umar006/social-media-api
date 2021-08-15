16eaa78e0532aed09a470846c1f8b2492d4535a3
umar006 <umaralfaruq597@gmail.com>
2021-08-15 15:49:51 +0700

    add save method
    to insert hashtags to database
    
    Signed-off-by: umar006 <umaralfaruq597@gmail.com>

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -9,6 +9,21 @@ class Hashtag
     @created_at = created_at
   end
 
+  def save
+    return false unless valid?
+
+    client = create_db_client
+    post_hashtag = PostHashtag.new
+
+    @hashtags.each do |hashtag|
+      client.query("insert into hashtags (hashtag, created_at) values ('#{hashtag.downcase}', str_to_date('#@created_at', '%d-%m-%Y %H:%i:%s'))")
+
+      post_hashtag.save
+    end
+
+    true
+  end
+
   def valid?
     return false if @hashtags.nil? || @hashtags.empty? || @created_at.nil?
 
