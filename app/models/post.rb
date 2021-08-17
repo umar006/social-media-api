f7b7b1c46a4e81ff4ca030dcc63c6798d0e47efb
umar006 <umaralfaruq597@gmail.com>
2021-08-17 13:47:19 +0700

    change user object class to username object string

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,13 +1,14 @@
 require './config/env/development'
+require './app/models/hashtag'
 require 'time'
 
 class Post
-  attr_accessor :post, :attach, :user, :created_at
+  attr_accessor :post, :attach, :username, :created_at
 
-  def initialize(post, attach=nil, created_at=Time.new, user)
+  def initialize(post, username, attach=nil, created_at=Time.new)
     @post = post
     @attach = attach
-    @user = user
+    @username = username
     @created_at = created_at.strftime("%d-%m-%Y %H:%M:%S")
   end
 
@@ -16,7 +17,7 @@ class Post
 
     client = create_db_client
 
-    client.query("insert into posts (post, attachment, created_at, username) values ('#@post', '#@attach', str_to_date('#@created_at', '%d-%m-%Y %H:%i:%s'), '#{@user.username}')")
+    client.query("insert into posts (post, attachment, created_at, username) values ('#@post', '#@attach', str_to_date('#@created_at', '%d-%m-%Y %H:%i:%s'), '#@username')")
 
     unless @post.scan(/#\w+/).empty?
       hashtags = Hashtag.new(@post.scan(/#\w+/), @created_at)
@@ -27,7 +28,7 @@ class Post
   end
 
   def valid?
-    return false if @post.nil? || @user.nil? || @post.size > 1000
+    return false if @post.nil? || @username.nil? || @post.size > 1000
 
     true
   end
