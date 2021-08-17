8d48ff3414dd8bfd3d9f59f2dc1e997034ce5e6b
umar006 <umaralfaruq597@gmail.com>
2021-08-17 21:04:05 +0700

    create convert method
    convert raw data from query sql to array
    
    Signed-off-by: umar006 <umaralfaruq597@gmail.com>

--- app/models/hashtag.rb
+++ app/models/hashtag.rb
@@ -4,7 +4,7 @@ require './app/models/post_hashtag'
 class Hashtag
   attr_accessor :hashtags, :created_at
 
-  def initialize(hashtags, created_at)
+  def initialize(hashtags, created_at=nil)
     @hashtags = hashtags
     @created_at = created_at
   end
@@ -17,20 +17,29 @@ class Hashtag
     @hashtags.each do |hashtag|
       client.query("insert into hashtags (hashtag, created_at) values ('#{hashtag.downcase}', str_to_date('#@created_at', '%d-%m-%Y %H:%i:%s'))")
 
-      save_to_hashtag
+      save_to_post_hashtag
     end
 
     true
   end
 
-  def save_to_hashtag
+  def save_to_post_hashtag
     post_hashtag = PostHashtag.new
     post_hashtag.save
   end
 
   def valid?
-    return false if @hashtags.nil? || @hashtags.empty? || @created_at.nil? || @created_at.empty?
+    return false if @hashtags.nil? || @hashtags.empty?
 
     true
+  end
+
+  def self.convert_to_array(raw_data)
+    hashtags = []
+    raw_data.each do |data|
+      hashtag = Hashtag.new(data['hashtag'])
+      hashtags << hashtag
+    end
+    hashtags
   end
 end
