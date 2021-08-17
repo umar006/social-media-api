a24397743ace5271e0344773cfe2880cc9c24de8
umar006 <umaralfaruq597@gmail.com>
2021-08-17 22:40:53 +0700

    create save method
    save comment to database and if have hashtags save to database as well
    
    Signed-off-by: umar006 <umaralfaruq597@gmail.com>

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -12,6 +12,22 @@ class Comment
     @created_at = created_at.strftime("%d-%m-%Y %H:%M:%S")
   end
 
+  def save
+    return false unless valid?
+
+    client = create_db_client
+
+    sql = "insert into comments (username, comment, attachment, created_at) " \
+        + "values ('#@username', '#@comment', '#@attachment', str_to_date('#@created_at', '%d-%m-%Y %H:%i:%s'))"
+    client.query(sql)
+
+    unless @post.scan(/#\w+/).empty?
+      save_to_hashtag
+    end
+
+    true
+  end
+
   def save_to_hashtag
     hashtags = Hashtag.new(@post.scan(/#\w+/), @created_at)
     hashtags.save
