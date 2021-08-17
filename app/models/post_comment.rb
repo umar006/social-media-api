ef3f6a93c0938ae48ccda4018c5f7a8ff7bd4ff3
umar006 <umaralfaruq597@gmail.com>
2021-08-17 22:47:56 +0700

    create save method
    this method will save post id and comment id to database
    
    Signed-off-by: umar006 <umaralfaruq597@gmail.com>

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,4 +1,15 @@
+require './config/env/development'
+
 class PostComment
+  def save
+    client = create_db_client
+
+    post_id = last_insert_id('posts')
+    comment_id = last_insert_id('comments')
+
+    client.query("insert into post_comments values ('#{post_id}', '#{comment_id}')")
+  end
+
   def last_insert_id(table_name)
     client = create_db_client
 
