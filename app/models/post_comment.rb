210f324945c00d310a46ddea192d02d40e8ba752
umar006 <umaralfaruq597@gmail.com>
2021-08-17 22:45:49 +0700

    create last insert id
    this method will search id from last insert
    
    Signed-off-by: umar006 <umaralfaruq597@gmail.com>

--- app/models/post_comment.rb
+++ app/models/post_comment.rb
@@ -1,2 +1,8 @@
 class PostComment
+  def last_insert_id(table_name)
+    client = create_db_client
+
+    id = client.query("select id from #{table_name} order by id desc").each.first['id']
+    id
+  end
 end
