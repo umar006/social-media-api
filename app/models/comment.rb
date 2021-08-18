bf939e8c6eccc7e0828b86c4971a4eed4a42c28d
umar006 <umaralfaruq597@gmail.com>
2021-08-18 12:33:32 +0700

    add post_id attribute

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,9 +4,10 @@ require './app/models/post_comment'
 require 'time'
 
 class Comment
-  attr_accessor :comment, :attachment, :created_at, :username
+  attr_accessor :comment, :attachment, :created_at, :username, :post_id
 
-  def initialize(username, comment, attachment=nil, created_at=Time.new)
+  def initialize(post_id, username, comment, attachment=nil, created_at=Time.new)
+    @post_id = post_id
     @username = username
     @comment = comment
     @attachment = attachment
@@ -18,8 +19,8 @@ class Comment
 
     client = create_db_client
 
-    sql = "insert into comments (username, comment, attachment, created_at) " \
-        + "values ('#@username', '#@comment', '#@attachment', str_to_date('#@created_at', '%d-%m-%Y %H:%i:%s'))"
+    sql = "insert into comments (post_id, username, comment, attachment, created_at) " \
+        + "values ('#@post_id', '#@username', '#@comment', '#@attachment', str_to_date('#@created_at', '%d-%m-%Y %H:%i:%s'))"
     client.query(sql)
 
     save_to_post_comment
