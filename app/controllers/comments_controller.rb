ca4495c3b12754a9d3bdcca004201077e6da5587
umar006 <umaralfaruq597@gmail.com>
2021-08-19 21:49:26 +0700

    create method create
    the method will save given parameters to comments table
    
    Signed-off-by: umar006 <umaralfaruq597@gmail.com>

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,8 @@
-require './app/controllers/comments_controller'
+require './app/models/comment'
 
 class CommentsController
-  
+  def create(params)
+    comment = Comment.new(params['post_id'], params['username'], params['comment'], params['attachment'])
+    comment.save
+  end
 end
---
+++ spec/controllers/comments_controller_spec.rb
@@ -0,0 +1,35 @@
+require './app/models/post'
+require './app/models/comment'
+require './app/controllers/comments_controller'
+require './config/env/test'
+
+describe CommentsController do
+  before(:each) do
+    client = create_db_client
+    client.query('delete from comments')
+    client.query('delete from posts')
+
+    post = Post.new("good morning", "umar2")
+    post.save
+    post_id = client.query("select id from posts where username='umar2'").each.first['id']
+
+    @valid_params = {
+      'post_id' => post_id,
+      'username' => 'umar2',
+      'comment' => 'good morning too',
+      'attachment' => nil
+    }
+    @comment = CommentsController.new
+  end
+
+  describe '#create' do
+    context 'with valid parameters' do
+      it 'save comment to database' do
+        @comment.create(@valid_params)
+
+        expected_comment = Comment.find_by_username('umar2')
+        expect(expected_comment).not_to be_nil
+      end
+    end
+  end
+end
