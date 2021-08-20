a34b028c594a4199c08aeb4e74d75e6524d7b9c8
umar006 <umaralfaruq597@gmail.com>
2021-08-20 11:53:08 +0700

    create routes to sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,25 @@
 require 'sinatra/base'
+require './app/controllers/users_controller'
+require './app/controllers/posts_controller'
+require './app/controllers/hashtags_controller'
+require './app/controllers/comments_controller'
 
 class Application < Sinatra::Base
-  
+  configure do
+    set :views, 'app/views'
+  end
+
+  get '/user' do
+    erb :'users/index'
+  end
+
+  get '/user/new' do
+    erb :'users/new'
+  end
+
+  post '/user/create' do
+    user = UsersController.new
+    user.create(params)
+    redirect '/user'
+  end
 end
