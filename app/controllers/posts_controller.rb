38f4b5be2150f3b661a04b33cfeaef0c36d7f3ba
umar006 <umaralfaruq597@gmail.com>
2021-08-20 15:16:46 +0700

    add statement for save file
    Signed-off-by: umar006 <umaralfaruq597@gmail.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,7 +2,13 @@ require './app/models/post'
 
 class PostsController
   def create(params)
-    post = Post.new(params['post'], params['username'])
+    post = Post.new(params['post'], params['username'], params['attachment'])
     post.save
+
+    filename = params[:file][:filename]
+    file = params[:file][:tempfile]
+    unless params['attachment'].nil? || params['attachment'].empty?
+      File.binwrite("./public/#{params['username']}_#{filename}", file.read)
+    end
   end
 end
