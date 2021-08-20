1e9696953009a265d5900b41ef644edfa6d49325
umar006 <umaralfaruq597@gmail.com>
2021-08-20 21:55:25 +0700

    create class method find by username
    Signed-off-by: umar006 <umaralfaruq597@gmail.com>

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,8 @@ class UsersController
     user = User.new(params['username'], params['email'], params['biodata'])
     user.save
   end
+
+  def self.find_by_username(username)
+    User.find_by_username(username)
+  end
 end
