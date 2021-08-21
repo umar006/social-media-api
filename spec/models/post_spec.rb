52d04839df90e47ebd33a41d2a5b4683dd5e626f
umar006 <umaralfaruq597@gmail.com>
2021-08-21 17:13:53 +0700

    test constructor with valid parameters
    expect return all mandatory attribute
    
    Signed-off-by: umar006 <umaralfaruq597@gmail.com>

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,10 +3,18 @@ require './app/models/user'
 
 describe Post do
   before(:each) do
-    user = User.new('umar', 'umar@gmail.com', nil)
-    @valid_post = Post.new('aku seorang kapiten', nil, user)
-    @invalid_post = Post.new(nil, nil, user)
-    @invalid_post_2 = Post.new("test" * 300, nil, user)
+    @valid_post = Post.new('aku seorang kapiten', "umar")
+    @invalid_post = Post.new(nil, "umar")
+    @invalid_post_2 = Post.new("test" * 300, "umar")
+  end
+
+  describe '#initialize' do
+    context 'with valid parameters' do
+      it 'return all mandatory atribute' do
+        expect(@valid_post.post).to eq('aku seorang kapiten')
+        expect(@valid_post.username).to eq('umar')
+      end
+    end
   end
 
   describe '#save' do
@@ -15,7 +23,7 @@ describe Post do
         stub = double
         allow(Mysql2::Client).to receive(:new).and_return(stub)
 
-        expect(stub).to receive(:query).with("insert into posts (post, attachment, created_at, username) values ('#{@valid_post.post}', '#{@valid_post.attach}', str_to_date('#{@valid_post.created_at}', '%d-%m-%Y %H:%i:%s'), '#{@valid_post.user.username}')")
+        expect(stub).to receive(:query).with("insert into posts (post, attachment, created_at, username) values ('#{@valid_post.post}', '#{@valid_post.attachment}', str_to_date('#{@valid_post.created_at}', '%d-%m-%Y %H:%i:%s'), '#{@valid_post.username}')")
         expect(@valid_post.save).to eq(true)
       end
     end
