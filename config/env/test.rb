87ed66049a57fda135645957b3b99c5f6da9c92a
umar006 <umaralfaruq597@gmail.com>
2021-08-25 00:06:26 +0700

    create method to clean tables and reset increment
    Signed-off-by: umar006 <umaralfaruq597@gmail.com>

--- config/env/test.rb
+++ config/env/test.rb
@@ -8,3 +8,18 @@ def create_db_client
     database: 'social_media_db_test'
   )
 end
+
+def clean_tables
+  create_db_client.query('delete from post_hashtags')
+  create_db_client.query('delete from hashtags')
+  create_db_client.query('delete from comments')
+  create_db_client.query('delete from posts')
+  create_db_client.query('delete from users')
+end
+
+def reset_increment
+  create_db_client.query('alter table users auto_increment = 1')
+  create_db_client.query('alter table posts auto_increment = 1')
+  create_db_client.query('alter table hashtags auto_increment = 1')
+  create_db_client.query('alter table comments auto_increment = 1')
+end
