6bab069156c86a40f035ad77e6b0884a79b2bc88
umar006 <umaralfaruq597@gmail.com>
2021-08-24 22:02:03 +0700

    test method valid?
    expect return false if one of the attributes nil
    
    Signed-off-by: umar006 <umaralfaruq597@gmail.com>

--- spec/models/post_hashtag_spec.rb
+++ spec/models/post_hashtag_spec.rb
@@ -39,5 +39,11 @@ describe PostHashtag do
         expect(@valid_post_hashtag.valid?).to eq(true)
       end
     end
+
+    context 'with invalid parameters' do
+      it 'return false if one of attributes nil' do
+        expect(@invalid_post_hashtag.valid?).to eq(false)
+      end
+    end
   end
 end
