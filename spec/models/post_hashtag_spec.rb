7659ab068786c719ad3d4a65f917cfad2533bd79
umar006 <umaralfaruq597@gmail.com>
2021-08-24 22:16:55 +0700

    test method save
    expect return true if given valid parameters
    
    Signed-off-by: umar006 <umaralfaruq597@gmail.com>

--- spec/models/post_hashtag_spec.rb
+++ spec/models/post_hashtag_spec.rb
@@ -1,3 +1,4 @@
+require './app/models/user'
 require './app/models/post'
 require './app/models/hashtag'
 require './app/models/post_hashtag'
@@ -12,6 +13,16 @@ describe PostHashtag do
     client.query('delete from posts')
     client.query('delete from users')
     client.query('alter table posts auto_increment = 1')
+    client.query('alter table hashtags auto_increment = 1')
+
+    user = User.new('umar', 'umar@gmail.com')
+    user.save
+
+    post = Post.new('good night #generasigigih', 'umar')
+    post.save
+
+    hashtag = Hashtag.new('generasigigih')
+    hashtag.save
     
     @valid_post_hashtag = PostHashtag.new(1, 1)
     @invalid_post_hashtag = PostHashtag.new(nil, nil)
@@ -43,6 +54,14 @@ describe PostHashtag do
     context 'with invalid parameters' do
       it 'return false if one of attributes nil' do
         expect(@invalid_post_hashtag.valid?).to eq(false)
+      end
+    end
+  end
+
+  describe '#save' do
+    context 'with valid parameters' do
+      it 'return true' do
+        expect(@valid_post_hashtag.save).to eq(true)
       end
     end
   end
