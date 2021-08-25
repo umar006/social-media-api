1a5f9cdb30c2717bb70a6647ed284d14df6fe4a9
umar006 <umaralfaruq597@gmail.com>
2021-08-25 07:40:25 +0700

    test class method find by username
    expect return user not nil
    
    Signed-off-by: umar006 <umaralfaruq597@gmail.com>

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,22 +7,29 @@ describe UsersController do
   before(:each) do
     client = create_db_client
     client.query('delete from users')
+
+    params = {
+      'username' => 'umar2',
+      'email' => 'umar2@gmail.com',
+      'biodata' => nil
+    }
+    user = UsersController.new
+    user.create(params)
   end
 
   describe '#create' do
     context 'when given valid parameters' do
       it 'save user' do
-        params = {
-          'username' => 'umar2',
-          'email' => 'umar2@gmail.com',
-          'biodata' => nil
-        }
-        user = UsersController.new
-        user.create(params)
 
         expected_user = User.find_by_username('umar2')
         expect(expected_user).not_to be_nil
       end
+    end
+  end
+
+  describe '#find_by_username' do
+    it 'not nil' do
+      expect(UsersController.find_by_username('umar2')).to_not be_nil
     end
   end
 end
