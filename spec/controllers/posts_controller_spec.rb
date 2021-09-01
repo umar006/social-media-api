399a4a491a11debd3da758e3800017a0dac015a7
umar006 <umaralfaruq597@gmail.com>
2021-09-01 07:55:03 +0700

    delete unused spec
    Signed-off-by: umar006 <umaralfaruq597@gmail.com>

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -1,7 +1,4 @@
 require 'test_helper'
-require './app/models/post'
-require './app/models/hashtag'
-require './app/models/post_hashtag'
 require './app/controllers/posts_controller'
 require './config/env/test'
 
@@ -9,20 +6,12 @@ describe PostsController do
   before(:each) do
     client = create_db_client
     client.query('delete from posts')
-    client.query('delete from hashtags')
-    client.query('delete from post_hashtags')
     client.query('alter table posts auto_increment = 1')
-    client.query('alter table hashtags auto_increment = 1')
-
-    hashtag = Hashtag.new('generasigigih')
-    hashtag.save
-
-    post_hashtag = PostHashtag.new(1, 1)
-    post_hashtag.save
 
     @params_valid = {
       'post' => 'good morning',
-      'username' => 'umar2'
+      'username' => 'umar2',
+      'id' => 1
     }
     @params_invalid = {
       'post' => nil,
@@ -49,12 +38,6 @@ describe PostsController do
   describe '#find_all' do
     it 'post equal to 1' do
       expect(PostsController.find_all.count).to eq(1)
-    end
-  end
-
-  describe '#find_by_hashtag' do
-    it 'post equal to 1' do
-      expect(PostsController.find_by_hashtag('generasigigih').count).to eq(1)
     end
   end
 
