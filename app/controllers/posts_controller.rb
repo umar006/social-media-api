9acbecd2f737bcbea401709c25d27780de5c8704
umar006 <umaralfaruq597@gmail.com>
2021-09-01 07:52:00 +0700

    add id to params
    Signed-off-by: umar006 <umaralfaruq597@gmail.com>

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,13 +3,13 @@ require './app/models/post'
 class PostsController
   def create(params)
     unless params['attachment'].nil?
-      post = Post.new(params['post'], params['username'], params['attachment']['filename'])
+      post = Post.new(params['post'], params['username'], params['attachment']['filename'], params['id'])
 
       filename = params['attachment']['filename']
       file = params['attachment']['tempfile']
       File.binwrite("./public/posts/#{params['username']}_#{filename}", file.read)
     else
-      post = Post.new(params['post'], params['username'])
+      post = Post.new(params['post'], params['username'], nil, params['id'])
     end
 
     post.save
