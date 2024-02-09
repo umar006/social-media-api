e3ba5148f9059b4a537b5177ecd5586d8eeea3e9
umar006 <umaralfaruq597@gmail.com>
2024-02-09 13:26:16 +0700

    Remove unused service

--- nestjs-drizzle/src/app.module.ts
+++ nestjs-drizzle/src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import databaseConfig from './config/database.config';
 import { DatabaseModule } from './database/database.module';
 
@@ -13,7 +11,5 @@ import { DatabaseModule } from './database/database.module';
     }),
     DatabaseModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
