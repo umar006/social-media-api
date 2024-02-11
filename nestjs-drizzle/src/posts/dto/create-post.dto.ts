305b0b9ce8469d1b69eaa295499fe95eec85b43c
umar006 <umaralfaruq597@gmail.com>
2024-02-11 20:07:36 +0700

    Add validation to create post dto

--- nestjs-drizzle/src/posts/dto/create-post.dto.ts
+++ nestjs-drizzle/src/posts/dto/create-post.dto.ts
@@ -1,4 +1,12 @@
+import { IsInt, IsNotEmpty, IsPositive, IsString } from 'class-validator';
+
 export class CreatePostDto {
+  @IsString()
+  @IsNotEmpty()
   content: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsNotEmpty()
   createdBy: number;
 }
