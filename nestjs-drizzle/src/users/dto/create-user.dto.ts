702fa86629d72d0b91c7aecfc48e4edf92b82c18
umar006 <umaralfaruq597@gmail.com>
2024-02-11 09:02:20 +0700

    Validate create user dto

--- nestjs-drizzle/src/users/dto/create-user.dto.ts
+++ nestjs-drizzle/src/users/dto/create-user.dto.ts
@@ -1,5 +1,15 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
+  @IsString()
+  @IsNotEmpty()
   username: string;
+
+  @IsString()
+  @IsNotEmpty()
   displayName: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
