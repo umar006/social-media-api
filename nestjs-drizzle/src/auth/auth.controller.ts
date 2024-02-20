84d3371be9ca8f9a611b2f10afae5b2e533330f6
umar006 <umaralfaruq597@gmail.com>
2024-02-20 22:37:22 +0700

    Create api to login

--- nestjs-drizzle/src/auth/auth.controller.ts
+++ nestjs-drizzle/src/auth/auth.controller.ts
@@ -1,7 +1,17 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { SignInDto } from './dto/sign-in.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  async signIn(@Body() signInDto: SignInDto) {
+    return await this.authService.signIn(
+      signInDto.username,
+      signInDto.password,
+    );
+  }
 }
--- nestjs-drizzle/src/auth/auth.service.ts
+++ nestjs-drizzle/src/auth/auth.service.ts
@@ -1,15 +1,36 @@
-import { Inject, Injectable } from '@nestjs/common';
+import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
 import {
   DRIZZLE_PROVIDER,
   DrizzlePostgres,
 } from 'src/database/providers/drizzle.provider';
 import { UsersService } from 'src/users/users.service';
+import bcrypt from 'bcrypt';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
     @Inject(DRIZZLE_PROVIDER)
     private readonly db: DrizzlePostgres,
   ) {}
+
+  async signIn(username: string, pass: string) {
+    const user =
+      await this.usersService.getUserByUsernameWithPassword(username);
+    const isValidated = await bcrypt.compare(pass, user.password);
+    if (!isValidated) {
+      throw new UnauthorizedException();
+    }
+
+    const payload = {
+      sub: user.id,
+      username: user.username,
+    };
+
+    const accessToken = await this.jwtService.signAsync(payload);
+
+    return { accessToken };
+  }
 }
