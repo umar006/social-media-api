8dfd7b238b8b8e659197b75960dcc4a1d79160b9
umar006 <umaralfaruq597@gmail.com>
2024-02-20 22:37:13 +0700

    Setup jwt

--- nestjs-drizzle/src/auth/auth.module.ts
+++ nestjs-drizzle/src/auth/auth.module.ts
@@ -1,10 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigType } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import authConfig from 'src/config/auth.config';
 import { UsersModule } from 'src/users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    JwtModule.registerAsync({
+      inject: [authConfig.KEY],
+      useFactory: (authCfg: ConfigType<typeof authConfig>) => {
+        return {
+          global: true,
+          secret: authCfg.JWT_SECRET,
+          signOptions: {
+            expiresIn: authCfg.JWT_EXPIRES,
+          },
+        };
+      },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
