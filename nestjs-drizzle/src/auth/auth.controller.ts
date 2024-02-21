3331d9801b6b754793a7a21c9101a46b24c72630
umar006 <umaralfaruq597@gmail.com>
2024-02-21 18:02:09 +0700

    Make api login public

--- nestjs-drizzle/src/auth/auth.controller.ts
+++ nestjs-drizzle/src/auth/auth.controller.ts
@@ -1,11 +1,13 @@
 import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { SignInDto } from './dto/sign-in.dto';
+import { Public } from './public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @HttpCode(HttpStatus.OK)
   @Post('login')
   async signIn(@Body() signInDto: SignInDto) {
