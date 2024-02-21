529c298857d86d47f664180e3a79f19093dc21b4
umar006 <umaralfaruq597@gmail.com>
2024-02-21 13:38:29 +0700

    Unused injection

--- nestjs-drizzle/src/auth/auth.service.ts
+++ nestjs-drizzle/src/auth/auth.service.ts
@@ -1,19 +1,13 @@
-import { Inject, Injectable, UnauthorizedException } from '@nestjs/common';
-import {
-  DRIZZLE_PROVIDER,
-  DrizzlePostgres,
-} from 'src/database/providers/drizzle.provider';
-import { UsersService } from 'src/users/users.service';
-import bcrypt from 'bcrypt';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import bcrypt from 'bcrypt';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class AuthService {
   constructor(
     private readonly usersService: UsersService,
     private readonly jwtService: JwtService,
-    @Inject(DRIZZLE_PROVIDER)
-    private readonly db: DrizzlePostgres,
   ) {}
 
   async signIn(username: string, pass: string) {
