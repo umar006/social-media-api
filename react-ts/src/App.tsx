946a75ad1c0673ca6ebef1ec4c8469a88c3f2c70
umar006 <umaralfaruq597@gmail.com>
2024-02-23 14:30:54 +0700

    Display login form if user not logged in

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,26 +1,50 @@
 import { useQuery } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import LoginForm from "./components/LoginForm";
 import PostForm from "./components/PostForm";
 import PostList from "./components/PostList";
 import { getAllPosts } from "./services/post";
 
 function App() {
+  const [token, setToken] = useState<string>();
+
   const { isPending, isError, data, error } = useQuery({
     queryKey: ["posts"],
     queryFn: getAllPosts,
   });
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("accessToken");
+    if (token) {
+      setToken(token);
+    }
+  }, []);
 
   if (isPending) {
     return <span>Loading...</span>;
   }
 
   if (isError) {
-    return <span>Erro: {error.message}</span>;
+    return <span>Error: {error.message}</span>;
   }
 
-  return (
+  const loginForm = () => (
+    <>
+      <h2>Login</h2>
+      <LoginForm setToken={setToken} />
+    </>
+  );
+
+  const postForm = () => (
     <>
       <h2>Create post</h2>
       <PostForm />
+    </>
+  );
+
+  return (
+    <>
+      {token ? postForm() : loginForm()}
       <h2>Posts</h2>
       <PostList posts={data} />
     </>
