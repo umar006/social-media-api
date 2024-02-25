2a0eac40907a8d8d54aca2a15167f7c836e9eef3
umar006 <umaralfaruq597@gmail.com>
2024-02-25 19:58:17 +0700

    Add helmet middleware

--- nestjs-drizzle/src/main.ts
+++ nestjs-drizzle/src/main.ts
@@ -1,10 +1,12 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
 
   app.setGlobalPrefix('api');
