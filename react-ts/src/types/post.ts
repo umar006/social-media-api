033167ac7e66e5d86b2e461d11d7f39e0dff0021
umar006 <umaralfaruq597@gmail.com>
2024-03-03 22:18:50 +0700

    Add image property to Post type

--- react-ts/src/types/post.ts
+++ react-ts/src/types/post.ts
@@ -1,5 +1,10 @@
 import { User } from "./user";
 
+interface PostImage {
+  id: string;
+  url: string;
+}
+
 export interface Post {
   id: string;
   createdAt: Date;
@@ -8,6 +13,7 @@ export interface Post {
   isLiked: boolean;
   likes: number;
   createdBy: User;
+  image?: PostImage;
 }
 
 export type NewPost = Pick<Post, "content">;
