89cf291bdf5e434f419b91e3e97c44e1aec28ae8
umar006 <umaralfaruq597@gmail.com>
2024-03-07 13:34:16 +0700

    Drop preload intent

--- react-ts/src/main.tsx
+++ react-ts/src/main.tsx
@@ -9,7 +9,6 @@ const router = createRouter({
   context: {
     queryClient,
   },
-  defaultPreload: "intent",
   // Since we're using React Query, we don't want loader calls to ever be stale
   // This will ensure that the loader is always called when the route is preloaded or visited
   defaultPreloadStaleTime: 0,
