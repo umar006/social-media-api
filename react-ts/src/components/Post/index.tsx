ec99068e2e1fad4068ab3e90791b9a91e6fa3783
umar006 <umaralfaruq597@gmail.com>
2024-03-13 21:39:55 +0700

    Style post

--- react-ts/src/components/Post/index.tsx
+++ react-ts/src/components/Post/index.tsx
@@ -54,19 +54,36 @@ function Post({ post }: Props) {
 
   return (
     <article>
-      <div>{post.createdBy.displayName}</div>
-      <div>@{post.createdBy.username}</div>
+      <div className="text-lg font-semibold">{post.createdBy.displayName}</div>
+      <div className="text-sm font-thin">@{post.createdBy.username}</div>
 
-      <div>
+      <div className="my-4">
         <p>{post.content}</p>
         {postImage()}
       </div>
 
-      <span>{likes}</span>
+      <div className="my-2 flex items-center gap-2">
+        <span className="text-lg font-medium">{likes}</span>
 
-      <button onClick={() => mutationLikes.mutate(post.id)}>
-        {isLiked ? "unlike" : "like"}
-      </button>
+        <button onClick={() => mutationLikes.mutate(post.id)}>
+          <svg
+            width="24"
+            height="21"
+            viewBox="0 0 24 21"
+            fill={isLiked ? "red" : "none"}
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M20.84 2.61C20.3292 2.099 19.7228 1.69364 19.0554 1.41708C18.3879 1.14052 17.6725 0.998173 16.95 0.998173C16.2275 0.998173 15.5121 1.14052 14.8446 1.41708C14.1772 1.69364 13.5708 2.099 13.06 2.61L12 3.67L10.94 2.61C9.9083 1.57831 8.50903 0.998708 7.05 0.998708C5.59096 0.998708 4.19169 1.57831 3.16 2.61C2.1283 3.64169 1.54871 5.04097 1.54871 6.5C1.54871 7.95903 2.1283 9.35831 3.16 10.39L4.22 11.45L12 19.23L19.78 11.45L20.84 10.39C21.351 9.87924 21.7563 9.27281 22.0329 8.60535C22.3095 7.93789 22.4518 7.22249 22.4518 6.5C22.4518 5.77751 22.3095 5.0621 22.0329 4.39464C21.7563 3.72718 21.351 3.12075 20.84 2.61V2.61Z"
+              stroke="red"
+              strokeOpacity="0.6"
+              strokeWidth="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </button>
+      </div>
 
       <p>{dateToUTC}</p>
     </article>
