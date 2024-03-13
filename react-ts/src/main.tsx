7546d4f30bdf1eb53e1f819188bc833993f138bf
umar006 <umaralfaruq597@gmail.com>
2024-03-13 10:15:17 +0700

    Setup tailwindcss

--- react-ts/src/main.tsx
+++ react-ts/src/main.tsx
@@ -2,6 +2,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider, createRouter } from "@tanstack/react-router";
 import ReactDOM from "react-dom/client";
 import { routeTree } from "./routeTree.gen.ts";
+import "./index.css";
 
 const queryClient = new QueryClient();
 const router = createRouter({
