a77c7995fd9471574d686b2bc9f2de0e89da3c48
umar006 <umaralfaruq597@gmail.com>
2024-03-14 10:21:05 +0700

    Handling error in posts route

--- react-ts/src/routes/posts/index.tsx
+++ react-ts/src/routes/posts/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
+import ErrorComponent from "../../components/Error";
 import { postListQueryOptions } from "../../components/PostList/postListQueryOptions";
 import postService from "../../services/post";
 
@@ -6,5 +7,13 @@ export const Route = createFileRoute("/posts/")({
   loader: async ({ context }) => {
     postService.setBearerToken(context.accessToken);
     return await context.queryClient.ensureQueryData(postListQueryOptions);
+  },
+  pendingComponent: () => <div>Loading...</div>,
+  errorComponent: () => {
+    return (
+      <ErrorComponent>
+        <div>Failed to fetch posts</div>
+      </ErrorComponent>
+    );
   },
 });
